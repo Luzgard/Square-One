67bd83ef0c9238e4516c5ad168f48dde80b8c757
Luzgard <corentin.denis@viacesi.fr>
2018-06-04 18:37:27 +0200

    FR to EN

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -4,17 +4,17 @@
     <div class="col-sm-offset-4 col-sm-4">
     	<br>
 		<div class="panel panel-primary">	
-			<div class="panel-heading">Fiche d'utilisateur</div>
+			<div class="panel-heading">User details</div>
 			<div class="panel-body"> 
-				<p>Nom : {{ $user->name }}</p>
-				<p>Email : {{ $user->email }}</p>
+				<p>Name : {{ $user->name }}</p>
+				<p>Mail : {{ $user->email }}</p>
 				@if($user->admin == 1)
-					Administrateur
+					Administrator
 				@endif
 			</div>
 		</div>				
 		<a href="javascript:history.back()" class="btn btn-primary">
-			<span class="glyphicon glyphicon-circle-arrow-left"></span> Retour
+			<span class="glyphicon glyphicon-circle-arrow-left"></span> Back
 		</a>
 	</div>
 @endsection
